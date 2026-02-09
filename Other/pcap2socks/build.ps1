51f6cebc22089df76c79579787d0d1b447a29c69
Boyce Lig <532762848@qq.com>
2026-02-09 18:49:25 +0800

    完成后清理文件

--- Other/pcap2socks/build.ps1
+++ Other/pcap2socks/build.ps1
@@ -5,10 +5,22 @@ try {
         -Uri 'https://github.com/zhxie/pcap2socks/releases/download/v0.6.2/pcap2socks-v0.6.2-windows-amd64.zip' `
         -OutFile 'pcap2socks.zip'
     Expand-Archive -Force -Path pcap2socks.zip -DestinationPath pcap2socks
+    
+    mv -Force 'pcap2socks\pcap2socks.exe' '..\release\pcap2socks.exe'
 }
 catch {
     exit 1
 }
+finally {
+    # 清理临时文件：包括下载的zip包 + 临时解压目录
+    if (Test-Path 'pcap2socks.zip') {
+        Remove-Item -Path 'pcap2socks.zip' -Force
+        Write-Host "Temporary file pcap2socks.zip cleaned up"
+    }
+    if (Test-Path 'pcap2socks') {
+        Remove-Item -Path 'pcap2socks' -Recurse -Force
+        Write-Host "Temporary unzip directory pcap2socks cleaned up"
+    }
+}
 
-mv -Force 'pcap2socks\pcap2socks.exe' '..\release\pcap2socks.exe'
 exit 0
